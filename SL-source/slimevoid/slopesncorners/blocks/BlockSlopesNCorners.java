7f2c75ff6995d714d6fe04b773117eedf258e3cb
Eurymachus <reflex_ion@hotmail.com>
2013-08-31 03:24:15 +0100

    Cleanup :D

--- SL-source/slimevoid/slopesncorners/blocks/BlockSlopesNCorners.java
+++ SL-source/slimevoid/slopesncorners/blocks/BlockSlopesNCorners.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
