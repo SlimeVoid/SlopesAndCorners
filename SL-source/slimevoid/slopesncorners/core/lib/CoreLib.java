be3e970c49842155935658deaa92d158ca60ba2e
Allen Cook <alcook16@gmail.com>
2013-09-11 23:24:53 -0400

    Update CoreLib
    tick version up 1 and change dependicy

--- SL-source/slimevoid/slopesncorners/core/lib/CoreLib.java
+++ SL-source/slimevoid/slopesncorners/core/lib/CoreLib.java
@@ -4,8 +4,8 @@ public class CoreLib {
 	
 	public static final String MOD_ID = "SlopesNCorners";
 	public static final String MOD_NAME = "Slimevoid Slopes 'N' Corners";
-	public static final String MOD_VERSION = "2.0.2.0";
-	public static final String MOD_DEPENDENCIES = "after:*";
+	public static final String MOD_VERSION = "2.0.2.1";
+	public static final String MOD_DEPENDENCIES = "required-after:SlimevoidLib;after:*";
 	public static final String MOD_CHANNEL = "SVSNC";
 	public static final String CLIENT_PROXY = "slimevoid.slopesncorners.client.proxy.ClientProxy";
 	public static final String COMMON_PROXY = "slimevoid.slopesncorners.proxy.CommonProxy";
