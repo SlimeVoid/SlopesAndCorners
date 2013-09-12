fc77a459e3b570606acf1e46664d3f446fd41a4c
Allen Cook <alcook16@gmail.com>
2013-09-12 10:16:21 -0400

    Max Tiles = 4

--- SL-source/slimevoid/slopesncorners/core/lib/BlockLib.java
+++ SL-source/slimevoid/slopesncorners/core/lib/BlockLib.java
@@ -3,7 +3,7 @@ package slimevoid.slopesncorners.core.lib;
 public class BlockLib {
 
 	public static final int MAX_DAMAGE = 4096;
-	public static final int MAX_TILES = 5;
+	public static final int MAX_TILES = 4;
 	public static final int BLOCK_SLOPES_ID = 0;
 	public static final int BLOCK_SIDES_ID = 1;
 	public static final int BLOCK_OBLICS_ID = 2;
